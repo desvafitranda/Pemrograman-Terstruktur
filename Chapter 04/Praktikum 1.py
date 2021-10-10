1e67a9e56720bb758c877a80cd22cb6fa8f3f2e0
desvafitranda <desvafitranda@student.uns.ac.id>
2021-10-10 12:42:48 +0700

    python

---
+++ Chapter 04/Praktikum 1.py
@@ -0,0 +1,11 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> type(10)
+<class 'int'>
+>>> type(10.1918)
+<class 'float'>
+>>> type('Hello World')
+<class 'str'>
+>>> type(True)
+<class 'bool'>
+>>> 
---
+++ Chapter 04/latihan05.py
@@ -0,0 +1,8 @@
+mahasiswa_laki = input("Mahasiswa Laki-laki: 100")
+MahasiswaLakilaki : 100
+mahasiswa_perempuan = input("Mahasiswa Perempuan: 150")
+MahasiswaPerempuan : 150
+diagram_laki = int(mahasiswa_laki) // 10
+diagram_perempuan = int(mahasiswa_perempuan) // 10
+print("Laki-laki :","*" * diagram_laki)
+print("Perempuan :","*" * diagram_perempuan)
