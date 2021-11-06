fd033a42a5f27c99b9b59bd0c5a8c677b95ee6b5
desvafitranda <89762116+desvafitranda@users.noreply.github.com>
2021-11-06 20:12:27 +0700

    Delete Chapter 07 directory

--- Chapter 07/latihan01.py
+++
@@ -1,7 +0,0 @@
-inputFile=input('Masukkan nama file: ')
-
-try:
-    file=open(inputFile, "r")
-    print('Isi file', inputFile, 'adalah: ', file.read())
-except:
-    print('File tidak ditemukan atau terdapat salah penulisan')
